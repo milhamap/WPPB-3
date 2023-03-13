b51f4e62b9d7f67bc7dcc259c6cbf62a224df107
milhamap <ilham3pratama@gmail.com>
2023-03-14 00:09:23 +0700

    Percobaan 3

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,20 @@ class DetailScreen extends StatelessWidget {
               ),
             ),
             Container(
+              margin: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: const <Widget>[
+                      Icon(Icons.calendar_today),
+                      Text('Open Everyday')
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Container(
               padding: const EdgeInsets.all(16.0),
               child: const Text(
                 'Museum inside a decommissioned Russian war submarine with tours & an adjacent park with cafes. Clean and well maintained. Car park cost 10k, entrance fee 15k/person. You can see KRI Pasopati there, it is a russian whiskey class. You can also watch the video about the Indonesian Navy at the building beside the submarine.',
